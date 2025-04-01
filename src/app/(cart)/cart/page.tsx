df134ea2d6bc94d278cf17eeb137527dbbab3a56
Burak Emre Küpçüoğlu <emrekupcuoglu@gmail.com>
2025-04-01 19:59:12 +0300

    updated dependencies and removed unused imports

--- src/app/(cart)/cart/page.tsx
+++ src/app/(cart)/cart/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 
 import CartItem from "@/features/Cart/CartItem";
 import CheckoutSummary from "@/features/Checkout/CheckoutSummary";
@@ -37,18 +37,8 @@ function CartPage() {
 
   return (
     <div className="mx-auto flex w-full flex-col pt-16">
-      {/* <div className="grid gap-24 pt-8"> */}
-      {/* <div className="grid grid-cols-2 pt-8 max-xl:grid-cols-2"> */}
       <div className="flex gap-4 max-md:flex-col">
         <Card className="bg-card-grey basis-full rounded-2xl border-none p-0">
-          {/* <CardHeader className="bg-background-hero rounded-t-2xl">
-            <div className="grid grid-cols-[3fr_1fr_1fr] gap-x-24 p-4 text-lg text-white">
-              <p className="justify-self-start">Product</p>
-
-              <p>Quantity</p>
-              <p className="justify-self-end">Total</p>
-            </div>
-          </CardHeader> */}
           <CardContent className="bg-card-grey rounded-b-2xl p-4">
             <div className="flex flex-col gap-4">
               {cart?.map((product, index) => {
