d77ebef05cc28bbc8d51bb8e8d8abc0d2da0abf7
Burak Emre Küpçüoğlu <emrekupcuoglu@gmail.com>
2025-04-03 16:05:38 +0300

    fixed new collection responsive bugs

--- src/features/HomePage/new-collection/NewCollection.tsx
+++ src/features/HomePage/new-collection/NewCollection.tsx
@@ -9,6 +9,7 @@ function NewCollection() {
     <div className="grid grid-cols-7 gap-8 max-md:grid-cols-1">
       <CollectionCard gridStyle="col-span-4 max-md:col-span-1">
         <div className="flex items-center justify-around">
+          {/* <div className="grid grid-cols-2 items-center justify-around"> */}
           <div className="">
             <span className="rounded-full bg-white px-3 py-2 font-bold uppercase">
               NEW COLLECTION
@@ -51,7 +52,7 @@ function NewCollection() {
           </div>
           <div className="pl-8">
             <Image
-              className="max-h-40 max-w-44 object-contain object-top max-md:h-24 max-md:w-24"
+              className="max-h-80 max-w-44 object-contain object-top max-md:h-44 max-md:w-44 max-sm:h-28 max-sm:w-28"
               src={chairImg}
               alt="Round table"
               sizes="(max-width: 768px) 100px, (max-width: 1200px) 150px, 200px"
@@ -60,27 +61,29 @@ function NewCollection() {
         </div>
       </CollectionCard>
       <CollectionCard gridStyle="col-span-2 h-full max-md:col-span-1">
-        <div className="flex h-full items-center">
-          <div className="grow">
-            <span className="rounded-full bg-white px-3 py-2 text-xs font-bold uppercase">
-              NEW COLLECTION
-            </span>
-            <div className="pl-3 pt-4">
-              <h1 className="">
-                <strong>Lamps</strong>
-              </h1>
-              <p>Tripod lamps</p>
-              <p>Study lamps</p>
-              <p>Table lamps</p>
+        <div className="flex h-full flex-col items-center">
+          <span className="rounded-full bg-white px-3 py-2 text-xs font-bold uppercase">
+            NEW COLLECTION
+          </span>
+          <div className="flex justify-between gap-4">
+            <div className="grow">
+              <div className="text-nowrap pl-3 pt-4">
+                <h1 className="">
+                  <strong>Lamps</strong>
+                </h1>
+                <p>Tripod lamps</p>
+                <p>Study lamps</p>
+                <p>Table lamps</p>
+              </div>
             </div>
-          </div>
-          <div className="">
-            <Image
-              className="max-h-24 max-w-24 object-contain max-md:h-24 max-md:w-24"
-              src={lampImg}
-              alt="Round table"
-              sizes="(max-width: 768px) 100px, (max-width: 1200px) 150px, 200px"
-            />
+            <div className="">
+              <Image
+                className="max-h-24 max-w-24 object-contain max-md:h-24 max-md:w-24"
+                src={lampImg}
+                alt="Round table"
+                sizes="(max-width: 768px) 100px, (max-width: 1200px) 150px, 200px"
+              />
+            </div>
           </div>
         </div>
       </CollectionCard>
